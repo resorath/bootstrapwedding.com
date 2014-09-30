e001321db44563292f286af235a0c5df27e9a9df
Sean Feil <sean@tophatandmonocle.com>
2014-09-29 21:07:20 -0600

    Responsiveness

--- application/views/home.php
+++ application/views/home.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>BootStrap Wedding - Modern Wedding Websites, Guest Management, Wedding Planning</title>
     <link href="<?=base_url() ?>assets/css/bootstrap.min.css" rel="stylesheet">
-    <link href="<?=base_url() ?>assets/css/core.css" rel="stylesheet">
+    <link href="<?=base_url() ?>assets/css/core.css" rel="stylesheet"> <!-- including desktop -->
+    <link href="<?=base_url() ?>assets/css/tablet.css" rel="stylesheet"> <!--or phablets or whatever -->
+    <link href="<?=base_url() ?>assets/css/phone.css" rel="stylesheet"> <!-- smart phones... razr not supported -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
