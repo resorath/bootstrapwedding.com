64f341403991ddd24bfb3274383500221b594c1f
Sean Feil <sean@tophatandmonocle.com>
2014-09-29 20:14:43 -0600

    Initial Commit

---
+++ application/views/home.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>BootStrap Wedding - Modern Wedding Websites, Guest Management, Wedding Planning</title>
+    <link href="<?=base_url() ?>assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?=base_url() ?>assets/css/core.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+      <div id="topbar">
+        <div class="container">
+          <div id="topbar-left">
+
+          </div>
+
+          <div id="topbar-right">
+            <div id="signin-button">
+              <a href="#">Login</a>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+
+      <div id="mainbar">
+        <div class="container" id="mainbar-container">
+          <h1>BootStrap Wedding</h1>
+
+
+          <span id="mainbar-headerlinks">
+            <ul>
+              <li><a href="#">Get Started</a></li>
+              <li><a href="#">How It Works</a></li>
+              <li><a href="#">FAQ</a></li>
+            </ul>
+          </span>
+        </div>
+      </div>
+
+      <div id="mainbanner">
+        <div id="banner">
+          <div class="container" id="banner-overlay-container">
+            <div id="banner-overlay">
+              <span class="" id="banner-shouter">
+                Kick start your wedding planning
+              </span>
+              <span class="" id="banner-follower">
+                We'll make the digital part easy, so you can focus on the hard parts.
+              </span>
+              <span id="banner-rear">
+                (Like which centrepiece, there are so many options!)
+              </span>
+              <div class="btn btn-primary btn-large" id="banner-action-button">Take the Tour!</div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+      <div id="followerbanner">
+        <ul>
+          <li><span class="glyphicon glyphicon-ok"></span> Modern Mobile First Wedding Website</li>
+          <li><span class="glyphicon glyphicon-ok"></span> Comprehensive Guest and RSVP Management</li>
+          <li><span class="glyphicon glyphicon-ok"></span> Pre-Wedding Event Planning</li>
+          <li><span class="glyphicon glyphicon-ok"></span> Crazy Customization Options</li>
+          <li><span class="glyphicon glyphicon-ok"></span> Total Control of Privacy</li>
+
+
+        </ul>
+
+        <p>... and so much more!</p>
+      </div>
+
+      <div id="howitworks">
+        <div class="container" id="howitworks-content">
+          <h2>How It Works</h2>
+
+          <p id="howitworks-message">
+            yolo
+          </p>
+        </div>
+
+      </div>
+
+      <div id="footer">
+
+      </div>
+
+
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="<?=base_url() ?>assets/js/jquery.backstretch.min.js"></script>
+    <script src="<?=base_url() ?>assets/js/bootstrap.min.js"></script>
+
+    <!--[if lt IE 9]>
+    <script>
+      $("#banner").backstretch("<?=base_url() ?>assets/img/bridge.jpg");
+    </script>
+    <![endif]-->
+  </body>
+</html>
