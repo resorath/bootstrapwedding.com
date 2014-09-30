94372ef10bcf326f38fa234efdadd70c5769113f
Sean Feil <sean@tophatandmonocle.com>
2014-09-29 23:18:09 -0600

    Design pass

--- application/views/home.php
+++ application/views/home.php
@@ -9,6 +9,10 @@
     <link href="<?=base_url() ?>assets/css/core.css" rel="stylesheet"> <!-- including desktop -->
     <link href="<?=base_url() ?>assets/css/tablet.css" rel="stylesheet"> <!--or phablets or whatever -->
     <link href="<?=base_url() ?>assets/css/phone.css" rel="stylesheet"> <!-- smart phones... razr not supported -->
+
+
+    <link href='http://fonts.googleapis.com/css?family=Comfortaa:400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -23,7 +27,7 @@
 
           <div id="topbar-right">
             <div id="signin-button">
-              <a href="#">Login</a>
+              <a href="#">Login to your Site</a>
             </div>
 
           </div>
@@ -33,7 +37,7 @@
 
       <div id="mainbar">
         <div class="container" id="mainbar-container">
-          <h1>BootStrap Wedding</h1>
+          <h1>Bootstrap Wedding</h1>
 
 
           <span id="mainbar-headerlinks">
