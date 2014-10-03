3a9cabe9d2ba4ee1475073e30fe9b6eb6388e814
Sean Feil <sean@tophatandmonocle.com>
2014-10-02 22:05:02 -0600

    responsive the second feature

--- application/views/home.php
+++ application/views/home.php
@@ -19,187 +19,180 @@
     <![endif]-->
   </head>
   <body>
-      <div id="topbar">
-        <div class="container">
-          <div id="topbar-left">
+    <div id="topbar">
+      <div class="container">
+        <div id="topbar-left">
 
+        </div>
+
+        <div id="topbar-right">
+          <div id="signin-button">
+            <a href="#">Login to your Site</a>
           </div>
 
-          <div id="topbar-right">
-            <div id="signin-button">
-              <a href="#">Login to your Site</a>
-            </div>
+        </div>
 
+      </div>
+    </div>
+
+    <div id="mainbar">
+      <div class="container" id="mainbar-container">
+        <h1>Bootstrap Wedding</h1>
+
+
+        <span id="mainbar-headerlinks">
+          <ul>
+            <li><a href="#">Get Started</a></li>
+            <li><a href="#">How It Works</a></li>
+            <li><a href="#">FAQ</a></li>
+          </ul>
+        </span>
+      </div>
+    </div>
+
+    <div id="mainbanner">
+      <div id="banner">
+        <div class="container" id="banner-overlay-container">
+          <div id="banner-overlay">
+            <span class="" id="banner-shouter">
+              Kick start your wedding planning
+            </span>
+            <span class="" id="banner-follower">
+              We'll make the digital part easy, so you can focus on the hard parts.
+            </span>
+            <span id="banner-rear">
+              (Like which centrepiece, there are so many options!)
+            </span>
+            <div class="btn btn-primary btn-large non-mobile-block-only" id="banner-action-button">Start with the "Custom You" Wedding Website</div>
+            <div class="btn btn-primary btn-large mobile-block-only" id="banner-action-button">Take a Peek</div>
           </div>
 
         </div>
       </div>
+    </div>
 
-      <div id="mainbar">
-        <div class="container" id="mainbar-container">
-          <h1>Bootstrap Wedding</h1>
-
-
-          <span id="mainbar-headerlinks">
-            <ul>
-              <li><a href="#">Get Started</a></li>
-              <li><a href="#">How It Works</a></li>
-              <li><a href="#">FAQ</a></li>
-            </ul>
-          </span>
-        </div>
-      </div>
-
-      <div id="mainbanner">
-        <div id="banner">
-          <div class="container" id="banner-overlay-container">
-            <div id="banner-overlay">
-              <span class="" id="banner-shouter">
-                Kick start your wedding planning
-              </span>
-              <span class="" id="banner-follower">
-                We'll make the digital part easy, so you can focus on the hard parts.
-              </span>
-              <span id="banner-rear">
-                (Like which centrepiece, there are so many options!)
-              </span>
-              <div class="btn btn-primary btn-large non-mobile-block-only" id="banner-action-button">Start with the "Custom You" Wedding Website</div>
-              <div class="btn btn-primary btn-large mobile-block-only" id="banner-action-button">Take a Peek</div>
-            </div>
-
-          </div>
-        </div>
-      </div>
-
-      <div id="followerbanner">
-        <ul>
-          <li><span class="glyphicon glyphicon-ok"></span> Modern Mobile-First Wedding Website</li>
-          <li><span class="glyphicon glyphicon-ok"></span> Comprehensive Guest and RSVP Management</li>
-          <li><span class="glyphicon glyphicon-ok"></span> Wedding Event Planning</li>
-          <li><span class="glyphicon glyphicon-ok"></span> Crazy Customization Options</li>
-          <li><span class="glyphicon glyphicon-ok"></span> Total Control of Privacy</li>
+    <div id="followerbanner">
+      <ul>
+        <li><span class="glyphicon glyphicon-ok"></span> Modern Mobile-First Wedding Website</li>
+        <li><span class="glyphicon glyphicon-ok"></span> Comprehensive Guest and RSVP Management</li>
+        <li><span class="glyphicon glyphicon-ok"></span> Wedding Event Planning</li>
+        <li><span class="glyphicon glyphicon-ok"></span> Crazy Customization Options</li>
+        <li><span class="glyphicon glyphicon-ok"></span> Total Control of Privacy</li>
 
 
-        </ul>
+      </ul>
 
-        <p>... and so much more!</p>
-      </div>
+      <p>... and so much more!</p>
+    </div>
 
-      <div id="howitworks">
-        <div class="container mainpage-container" id="howitworks-content">
-          <h2>How It Works</h2>
+    <div id="howitworks">
+      <div class="container mainpage-container" id="howitworks-content">
+        <h2>How It Works</h2>
 
-          <p id="howitworks-message">
-            Your wedding website is your most important tool in communicating with your guests, it should be as thoughtfully crafted as your wedding.
-            Start with a timeless template, then match the colours to the rest of your wedding theme. Customize away! Then dive into the easy to use, powerful 
-            tools we provide to invite, communicate with, and delight your guests.
+        <p id="howitworks-message">
+          Your wedding website is your most important tool in communicating with your guests, it should be as thoughtfully crafted as your wedding.
+          Start with a timeless template, then match the colours to the rest of your wedding theme. Customize away! Then dive into the easy to use, powerful 
+          tools we provide to invite, communicate with, and delight your guests.
 
-          </p>
+        </p>
 
 
-          <div class="row" id="howitworks-cols">
+        <div class="row" id="howitworks-cols">
 
-            <div class="col-sm-3">
-              <img src="http://soapersstreet.tophatandmonocle.com/assets/img/signup.png">
-              <h3>Pick</h3>
-              <p>Choose a template, choose some colours, decide which pages make sense to you.</p>
-            </div>
-
-            <div class="col-sm-3">
-              <img src="http://soapersstreet.tophatandmonocle.com/assets/img/signup.png">
-              <h3>Populate</h3>
-              <p>Add details, events, pictures, directions, or whatever information your guests need.</p>
-            </div>
-
-            <div class="col-sm-3">
-              <img src="http://soapersstreet.tophatandmonocle.com/assets/img/signup.png">
-              <h3>Invite</h3>
-              <p>Facebook, spreadsheets, or our own intuitive interface.</p>
-            </div>
-            
-            <div class="col-sm-3">
-              <img src="http://soapersstreet.tophatandmonocle.com/assets/img/signup.png">
-              <h3>Delight</h3>
-              <p>Keep your guests up to date with the latest stories, photos, places to eat, and things to see.</p>
-            </div>
-
+          <div class="col-sm-3">
+            <img src="<?=base_url() ?>assets/img/howitworks-pick.png">
+            <h3>Pick</h3>
+            <p>Choose a template, choose some colours, decide which pages make sense to you.</p>
           </div>
 
-        </div>
-      </div>
-
-      <p>
-
-
-      </div>
-
-      <div id="features-super-container">
-
-        <div class="features">
-          <div class="features-image" id="features-website-left">
+          <div class="col-sm-3">
+            <img src="<?=base_url() ?>assets/img/howitworks-populate.png">
+            <h3>Populate</h3>
+            <p>Add details, events, pictures, directions, or whatever information your guests need.</p>
           </div>
-          <div class="features-text" id="features-website-right">
-            <h2>Start with your Website</h2>
-            <p>Your guests don't sit at their computer anymore. Your wedding website should work flawlessly across phones, tablets, and computers. 
-            All our designs are hand crafted to look timeless across everyone's device. There's nothing special you have to do, it just works!</p> 
+
+          <div class="col-sm-3">
+            <img src="<?=base_url() ?>assets/img/howitworks-invite.png">
+            <h3>Invite</h3>
+            <p>Facebook, spreadsheets, or our own intuitive interface.</p>
+          </div>
           
-            <div id="mockups-container">
-              <div class="one-third-stacking-col">
-                <img src="<?=base_url() ?>assets/img/imac.png" class="mockup" id="mockup-imac">
-                <em class="figure">Design1 Name</em>
-              </div>
-              <div class="one-third-stacking-col">
-                <img src="<?=base_url() ?>assets/img/iphone.png" class="mockup" id="mockup-iphone">
-                <em class="figure">Design2 Name</em>
-              </div>
-              <div class="one-third-stacking-col">
-                <img src="<?=base_url() ?>assets/img/ipad.png" class="mockup" id="mockup-ipad">
-                <em class="figure">Design3 Name</em>
-              </div>
-            </div>
-
-            <div class="btn btn-primary btn-features-cta" id="btn-features-cta-website">View More Designs</div>
-
-
+          <div class="col-sm-3">
+            <img src="<?=base_url() ?>assets/img/howitworks-delight.png">
+            <h3>Delight</h3>
+            <p>Keep your guests up to date with the latest stories, photos, places to eat, and things to see.</p>
           </div>
-
-          <div class="features-footer mobile-block-only"></div>
 
         </div>
 
-        <div class="features">
+      </div>
+    </div>
 
-          <div class="features-text" id="features-customize-left">
-            <h2>Then Make it Yours</h2>
-   
+    <div id="features-super-container">
 
-            <p>Use our powerful, intuitive editor to add as much or as little content as you like. Pictures, stories, schedules, things to do, places to eat, whatever you like! Then change the colours, add pages, delete pages, change the colours again. Change all the things!</p> 
-       
-
-           <div class="btn btn-primary btn-features-cta" id="btn-features-cta-customize">Try the Editor</div>
-          </div>
-
-          <div class="features-image" id="features-customize-right">
-            <div id="features-video-container">
-              <div id="features-video-embed">
-                <iframe width="560" height="315" src="//www.youtube.com/embed/FsjwG3FQna4" frameborder="0" allowfullscreen id="features-video-embed-content"></iframe>
-              </div>
+      <div class="features">
+        <div class="features-image" id="features-website-left">
+        </div>
+        <div class="features-text" id="features-website-right">
+          <h2>Start with your Website</h2>
+          <p>Your guests don't sit at their computer anymore. Your wedding website should work flawlessly across phones, tablets, and computers. 
+          All our designs are hand crafted to look timeless across everyone's device. There's nothing special you have to do, it just works!</p> 
+        
+          <div id="mockups-container">
+            <div class="one-third-stacking-col">
+              <img src="<?=base_url() ?>assets/img/imac.png" class="mockup" id="mockup-imac">
+              <em class="figure">Design1 Name</em>
+            </div>
+            <div class="one-third-stacking-col">
+              <img src="<?=base_url() ?>assets/img/iphone.png" class="mockup" id="mockup-iphone">
+              <em class="figure">Design2 Name</em>
+            </div>
+            <div class="one-third-stacking-col">
+              <img src="<?=base_url() ?>assets/img/ipad.png" class="mockup" id="mockup-ipad">
+              <em class="figure">Design3 Name</em>
             </div>
           </div>
 
-          <div class="features-footer mobile-block-only"></div>
+          <div class="btn btn-primary btn-features-cta" id="btn-features-cta-website">View More Designs</div>
+
 
         </div>
 
+        <div class="features-footer mobile-block-only"></div>
 
       </div>
 
-      <div id="footer">
+      <div class="features">
+
+        <div class="features-text" id="features-customize-left">
+          <h2>Then Make it Yours</h2>
+ 
+
+          <p>Use our powerful, intuitive editor to add as much or as little content as you like. Pictures, stories, schedules, things to do, places to eat, whatever you like! Then change the colours, add pages, delete pages, change the colours again. Change all the things!</p> 
+     
+
+         <div class="btn btn-primary btn-features-cta" id="btn-features-cta-customize">Try the Editor</div>
+        </div>
+
+        <div class="features-image" id="features-customize-right">
+          <div id="features-video-container">
+            <div id="features-video-embed">
+              <iframe width="560" height="315" src="//www.youtube.com/embed/FsjwG3FQna4" frameborder="0" allowfullscreen id="features-video-embed-content"></iframe>
+            </div>
+          </div>
+        </div>
+
+        <div class="features-footer mobile-block-only"></div>
 
       </div>
 
 
     </div>
+
+    <div id="footer">
+
+    </div>
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="<?=base_url() ?>assets/js/jquery.backstretch.min.js"></script>
     <script src="<?=base_url() ?>assets/js/bootstrap.min.js"></script>
