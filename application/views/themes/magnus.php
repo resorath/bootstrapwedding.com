55d97411cfa1a809a6d572cb07a2c286dfb6e1fd
Sean Feil <sfeil@c4349b.local>
2014-10-09 22:37:56 -0600

    Basic templates

---
+++ application/views/themes/magnus.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, minimal-ui, user-scalable=no">
+    <title>Bootstrap Wedding Theme 1 Magnus</title>
+    <link href="<?=base_url() ?>assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?=base_url() ?>assets/css/core.css" rel="stylesheet"> <!-- including desktop -->
+    <link href="<?=base_url() ?>assets/css/tablet.css" rel="stylesheet"> <!--or phablets or whatever -->
+    <link href="<?=base_url() ?>assets/css/phone.css" rel="stylesheet"> <!-- smart phones... razr not supported -->
+
+
+    <link href='//fonts.googleapis.com/css?family=Comfortaa:400,700' rel='stylesheet' type='text/css'>
+    <link href='//fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    <link href='//fonts.googleapis.com/css?family=Lato:400,700,900' rel='stylesheet' type='text/css'>
+    <link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    </head>
+    <body>
+        <div class="navigation">
+            <ul>
+                <li>Us</li>
+                <li>The Day</li>
+                <li>Location</li>
+                <li>Accomodations</li>
+                <li>Travel</li>
+                <li>Registry</li>
+                <li>RSVP</li>
+                <li>Ask us Something</li>
+
+
+            </ul>
+        </div>
+
+        <div class="content">
+
+        </div>
+
+        <div class="footer">
+
+        </div>
+
+
+    </body>
+</html>
